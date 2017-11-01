64e1037f0cbeb29727ba5c4230b309ce660b8649
Mike Stowe <SnoopSqueak@aim.com>
2017-11-01 16:10:37 -0500

    Added (empty) Game to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Game from './components/Game.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Game />
       </div>
     );
   }
---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <p>There's nothing here just yet. Please be patient! ;)</p>
+    );
+  }
+}
+
+export default Game;
