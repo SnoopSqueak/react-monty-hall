3f7be4f451dbfe235ba45b70333ec58cf8624144
Mike Stowe <SnoopSqueak@aim.com>
2017-11-14 11:06:11 -0600

    Refactored, flipped doors, finished random goats

--- src/components/Door.js
+++ src/components/Door.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
 
 class Door extends Component {
-  constructor(props) {
-    super(props);
-    let randNum = (Math.floor(Math.random() * 6) + 1);
-    if (randNum >= 3) {
-      randNum = 1;
-    } else if (randNum > 1) {
-      randNum = 2;
-    } else {
-      randNum = 3;
-    }
-    this.state = {
-      variation: randNum
-    }
-  }
-
   render() {
     let doorClass = this.props.isOpen ? "door-open" : "door-closed";
-    let prizeClass = this.props.hasPrize ? "prize" : "zonk zonk" + this.state.variation;
+    let prizeClass = this.props.content;
     let parentClass = "door";
     if (this.props.isSelected) parentClass += " door-selected";
     if (!this.props.isOpen) {
@@ -40,7 +25,7 @@ class Door extends Component {
 Door.defaultProps = {
   number: 0,
   isSelected: false,
-  hasPrize: false,
+  content: "zonk1",
   isOpen: false,
   clickHandler: () => {
     console.log("No click handler has been set up for this door.");
